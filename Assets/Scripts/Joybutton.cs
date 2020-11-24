b1b2235d460de7e84167d4b0b0ff1209302716dd
Marko Paunovic <marko.paunovic1998@gmail.com>
2020-11-24 02:46:42 +0100

    Basic movement

---
+++ Assets/Scripts/Joybutton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Joybutton : MonoBehaviour, IPointerDownHandler,IPointerUpHandler
+{
+    [HideInInspector]
+    protected bool pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    protected FixedJoystick joystick;
+    protected Joybutton joybutton;
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    private void Start()
+    {
+        joystick = FindObjectOfType<FixedJoystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+    private void Update()
+    {
+        rb.velocity = new Vector2(joystick.Horizontal * moveSpeed * Time.fixedDeltaTime, joystick.Vertical * moveSpeed * Time.fixedDeltaTime);
+    }
+}
